1f005fc4eb31f14392bc2a2b897116dba9bf887b
espeniel76 <espeniel69@gmail.com>
2021-07-29 09:09:49 +0900

    state 만들기

---
+++ hello_world/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+// import Hello from "./components/state1";
+import Hello from "./components/state2";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>state</h1>
+			<Hello></Hello>
+			<Hello></Hello>
+			<Hello></Hello>
+		</div>
+	);
+}
+
+export default App;
---
+++ hello_world/src/components/state2.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export default function Hello() {
+	const [name, setName] = useState("Mike");
+
+	return (
+		<div>
+			<h2 id="name">{name}</h2>
+			<button
+				onClick={() => {
+					setName(name === "Mike" ? "Jane" : "Mike");
+				}}
+			>
+				Change
+			</button>
+		</div>
+	);
+}
