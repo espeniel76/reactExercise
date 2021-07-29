d870505d21c535183053b71572036e427d4c47f6
espeniel76 <espeniel69@gmail.com>
2021-07-30 08:47:21 +0900

    react ajax

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,16 +1,43 @@
 import "./App.css";
 // import Hello from "./components/state1";
-import Hello from "./components/state2";
+// import Hello from "./components/state2";
+import List from "./components/List";
+import { Component, useState, useEffect } from "react";
 
-function App() {
+// function App() {
+// 	return (
+// 		<div className="App">
+// 			<h1>state</h1>
+// 			<Hello age={10}></Hello>
+// 			<Hello age={20}></Hello>
+// 			<Hello age={30}></Hello>
+// 		</div>
+// 	);
+// }
+
+const App = () => {
+	const [todos, setTodos] = useState(["js공부"]);
+	const [newTodo, setNewTodo] = useState();
+
+	const changeInputData = (e) => {
+		setNewTodo(e.target.value);
+	};
+	const addTodo = (e) => {
+		setTodos([...todos, newTodo]);
+	};
+	useEffect(() => {
+		console.log("새로운 내용이 렌더링됐네요", todos);
+	}, [todos]);
 	return (
-		<div className="App">
-			<h1>state</h1>
-			<Hello></Hello>
-			<Hello></Hello>
-			<Hello></Hello>
+		<div>
+			<h1>todo 애플리케이션</h1>
+
+			<input type="text" name="" onChange={changeInputData}></input>
+			<button onClick={addTodo}>할일추가</button>
+
+			<List todos={todos}></List>
 		</div>
 	);
-}
+};
 
 export default App;
--- hello_world/src/components/state2.js
+++ hello_world/src/components/state2.js
@@ -1,14 +1,19 @@
 import { useState } from "react";
+import UserName from "./UserName";
 
-export default function Hello() {
+export default function Hello(props) {
 	const [name, setName] = useState("Mike");
-
+	const [age, setAge] = useState(props.age);
 	return (
 		<div>
-			<h2 id="name">{name}</h2>
+			<h2 id="name">
+				{name}({age}세)
+			</h2>
+			<UserName name={name}></UserName>
 			<button
 				onClick={() => {
 					setName(name === "Mike" ? "Jane" : "Mike");
+					setAge(age + 1);
 				}}
 			>
 				Change
